61491e9ca3d28c6dc733b8dbb8682a42f14bb88d
exarkun <>
2006-05-28 18:36:32 +0000

    Merge optional-ssh-1061
    Authors: exarkun, radix
    Reviewer: idnar
    Fixes #1061
    
    This guards imports of Twisted ssh modules in case they fail due to
    lack of PyCrypto and gracefully disables ssh functionality in these
    cases.

--- xmantissa/plugins/imaginaryoff.py
+++ xmantissa/plugins/imaginaryoff.py
@@ -7,15 +7,23 @@ Mantissa Offering Plugin for Imaginary
 from xmantissa import offering
 
 from imaginary import iimaginary
-from imaginary.wiring import realm, telnet, ssh
+from imaginary.wiring import realm, telnet
+
+siteRequirements = [(iimaginary.ITelnetService, telnet.TelnetService)]
+try:
+    from imaginary.wiring import ssh
+except ImportError:
+    pass
+else:
+    siteRequirements.append((
+        iimaginary.ISSHService, ssh.SSHService))
 
 imaginaryOffering = offering.Offering(
     name = u"imaginary",
     description = u"""
     A Text Adventure!  Kill the dragon!  Kill it!!!
     """,
-    siteRequirements = [(iimaginary.ITelnetService, telnet.TelnetService),
-                        (iimaginary.ISSHService, ssh.SSHService)],
+    siteRequirements = siteRequirements,
     appPowerups = [realm.ImaginaryRealm],
     benefactorFactories = [],
     loginInterfaces = [(iimaginary.IActor, "Imaginary logins")],
