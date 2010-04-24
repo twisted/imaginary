22c32e47ccc26b980f5d3cd31503a15fc1c562d0
exarkun <>
2010-04-24 16:54:44 +0000

    Add a test for falling back if there's no name attribute

--- imaginary/test/test_idea.py
+++ imaginary/test/test_idea.py
@@ -15,6 +15,11 @@ class Named(record('name')):
     pass
 
 
+class Reprable(record('repr')):
+    def __repr__(self):
+        return self.repr
+
+
 class PathTests(TestCase):
     """
     Tests for L{imaginary.idea.Path}.
@@ -32,3 +37,20 @@ class PathTests(TestCase):
             "Path(\n"
             "\t'office' => 'desk' []\n"
             "\t'desk' => 'monitor' [])")
+
+
+    def test_unnamedDelegate(self):
+        """
+        The I{repr} of a L{Path} containing delegates without names includes the
+        I{repr} of the delegates.
+        """
+        monitor = Idea(Reprable("monitor"))
+        desk = Idea(Reprable("desk"))
+        office = Idea(Reprable("office"))
+        path = Path([Link(office, desk), Link(desk, monitor)])
+        self.assertEquals(
+            repr(path),
+            "Path(\n"
+            "\toffice => desk []\n"
+            "\tdesk => monitor [])")
+
