e0d9e71ee96fef0e1347c49cf4a76eef15d62885
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2014-05-17 18:17:02 -0400

    Generate the versioneer artifacts with `setup.py versioneer` - necessary only when first switching to python-versioneer or when upgrading to a new version of python-versioneer.

--- imaginary/__init__.py
+++ imaginary/__init__.py
@@ -17,3 +17,7 @@ creation                        # exported
 # Ideally there would be a nice, passive way to register verbs which would only
 # load them as necessary rather than forcing the entire package to get
 # imported, but this will work okay for now.
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
