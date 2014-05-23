299205b61dedc2f4aabc5f6bcb1eb98e5f9ff539
Glyph <glyph@twistedmatrix.com>
2014-05-22 18:06:29 -0700

    Add just enough setup for ExampleGame that it can be installed into the same virtualenv.

---
+++ ExampleGame/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup
+
+setup(
+    name="ExampleGame",
+    packages=["examplegame",
+              "examplegame.test",
+              "imaginary.plugins"],
+    version="0.0.0",
+)
