ed4175f1bf972e0dbd99f37dc9d5fc3d37628d2f
Glyph <glyph@twistedmatrix.com>
2014-06-21 12:19:04 -0700

    These tests really ought to have been in Twisted.

--- imaginary/__main__.py
+++ imaginary/__main__.py
@@ -4,18 +4,23 @@ Run a single-player Imaginary world.
 from __future__ import unicode_literals
 
 import fcntl
+import termios
 import struct
 import tty
+import sys
+import os
 import signal
 
 from axiom.store import Store
 
 from twisted.python.log import startLogging
+from twisted.internet.stdio import StandardIO
+from twisted.conch.insults.insults import ServerProtocol
+from twisted.internet.task import react
 
 from imaginary.wiring.player import Player
 from imaginary.world import ImaginaryWorld
 from imaginary.wiring.terminalui import TextServerBase
-from twisted.conch.stdio import runWithProtocol
 
 def getTerminalSize(terminalFD):
     """
@@ -43,9 +48,10 @@ class ConsoleTextServer(TextServerBase, object):
     """
     state = b'COMMAND'
 
-    def __init__(self, player, terminalFD):
+    def __init__(self, player, terminalFD, done):
         self.player = player
         self.terminalFD = terminalFD
+        self.done = done
 
     def connectionMade(self):
         super(ConsoleTextServer, self).connectionMade()
@@ -54,25 +60,58 @@ class ConsoleTextServer(TextServerBase, object):
         self.terminalSize(width, height)
 
     def connectionLost(self, reason):
-        from twisted.internet import reactor
-        reactor.stop()
+        self.done.callback(None)
 
 
-def main():
-    startLogging(file('imaginary-debug.log', 'w'))
 
+def makeTextServer(reactor):
     store = Store()
     world = ImaginaryWorld(store=store)
     actor = world.create("player")
 
-    def makeTextServer():
-        tsb = ConsoleTextServer(Player(actor), sys.stdout.fileno())
-        from twisted.internet import reactor
-        def winchAccess(signum, frame):
-            reactor.callFromThread(tsb.terminalSize, *getTerminalSize()[::-1])
-        signal.signal(signal.SIGWINCH, winchAccess)
-        return tsb
-    runWithProtocol(makeTextServer)
+    tsb = ConsoleTextServer(Player(actor), sys.stdout.fileno())
+    def winchAccess(signum, frame):
+        reactor.callFromThread(tsb.terminalSize, *getTerminalSize()[::-1])
+    signal.signal(signal.SIGWINCH, winchAccess)
+    return tsb
 
-if __name__ == '__main__':
-    main()
+
+
+def runTextServer(reactor):
+    """
+    Run a L{ConsoleTextServer}.
+    """
+    textServer = makeTextServer(reactor)
+    StandardIO(ServerProtocol(lambda: textServer))
+    return textServer.done
+
+
+CLEAR_SCREEN = b"\r\x1bc\r"
+
+def withSavedTerminalSettings(fd, blockingCall):
+    """
+    Save and restore terminal settings for the given file descriptor.
+    """
+    oldSettings = termios.tcgetattr(fd)
+    tty.setraw(fd)
+    try:
+        blockingCall()
+    finally:
+        termios.tcsetattr(fd, termios.TCSANOW, oldSettings)
+        os.write(fd, CLEAR_SCREEN)
+
+
+
+def ifmain(thunk):
+    if __name__ == '__main__':
+        thunk(sys.argv[1:])
+    return thunk
+
+
+
+@ifmain
+def main(argv):
+    startLogging(file('imaginary-debug.log', 'w'))
+    withSavedTerminalSettings(sys.__stdin__.fileno(),
+                              lambda: react(runTextServer, argv))
+
--- imaginary/test/test_runner.py
+++ imaginary/test/test_runner.py
@@ -7,15 +7,20 @@ import fcntl
 import pty
 import tty
 import struct
+import termios
 
 from axiom.store import Store
 
 from twisted.trial.unittest import TestCase
+
+from twisted.internet.defer import Deferred
 from twisted.test.proto_helpers import StringTransport
 from twisted.conch.insults.insults import ServerProtocol
 
 from imaginary.world import ImaginaryWorld
 from imaginary.wiring.player import Player
+from imaginary.__main__ import withSavedTerminalSettings
+from imaginary.__main__ import CLEAR_SCREEN
 from imaginary.__main__ import getTerminalSize, ConsoleTextServer
 
 
@@ -67,10 +72,38 @@ class WindowSizing(TestCase):
 
         setTerminalSize(leader, 234, 567)
         player = Player(actor)
-        textServer = ConsoleTextServer(player, follower)
+        textServer = ConsoleTextServer(player, follower, Deferred())
         terminalProtocol = ServerProtocol(lambda: textServer)
         self.assertIdentical(player.proto, None)
         terminalProtocol.makeConnection(StringTransport())
         self.assertIdentical(player.proto, textServer)
         self.assertEqual(textServer.width, 567)
         self.assertEqual(textServer.height, 234)
+
+
+    def test_withSavedTerminalSettings(self):
+        """
+        L{withSavedTerminalSettings} saves and then restores the settings for
+        the terminal that it is given.
+        """
+        leader, follower = makeTerminal(self)
+
+        attrs = termios.tcgetattr(follower)
+        [iflag, oflag, cflag, lflag, ispeed, ospeed, cc] = attrs
+        def mangle():
+            os.write(follower, b"HELLO")
+            termios.tcsetattr(follower, termios.TCSANOW,
+                              [iflag, oflag, cflag, lflag,
+                               ispeed + 1000, ospeed + 1000, cc])
+            mangle.run = True
+        mangle.run = False
+        withSavedTerminalSettings(follower, mangle)
+        self.assertEqual(mangle.run, True)
+        newattrs = termios.tcgetattr(follower)
+        LFLAGS = 3
+        # These seem to change randomly, and I'm not sure why, so let's exclude
+        # them from the test (we never changed them in mangle() anyway).
+        newattrs[LFLAGS] = 0
+        attrs[LFLAGS] = 0
+        self.assertEqual(newattrs, attrs)
+        self.assertEqual(os.read(leader, 1024), b"HELLO" + CLEAR_SCREEN)
