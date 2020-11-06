d4f4d2252816521c626dd7bc0ccac053e2a56346
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-11-05 20:54:28 -0500

    expand docstring with the spec

--- CloakOfDarkness/cloakofdarkness/__init__.py
+++ CloakOfDarkness/cloakofdarkness/__init__.py
@@ -1,6 +1,32 @@
 
 """
 An Imaginary implementation of <http://www.firthworks.com/roger/cloak/>.
+
+Quoting:
+
+* The Foyer of the Opera House is where the game begins. This empty room has
+  doors to the south and west, also an unusable exit to the north. There is
+  nobody else around.
+
+* The Bar lies south of the Foyer, and is initially unlit. Trying to do
+  anything other than return northwards results in a warning message about
+  disturbing things in the dark.
+
+* On the wall of the Cloakroom, to the west of the Foyer, is fixed a small
+  brass hook.
+
+* Taking an inventory of possessions reveals that the player is wearing a
+  black velvet cloak which, upon examination, is found to be
+  light-absorbent. The player can drop the cloak on the floor of the Cloakroom
+  or, better, put it on the hook.
+
+* Returning to the Bar without the cloak reveals that the room is now lit. A
+  message is scratched in the sawdust on the floor.
+
+* The message reads either "You have won" or "You have lost", depending on how
+  much it was disturbed by the player while the room was dark.
+
+* The act of reading the message ends the game.
 """
 
 from __future__ import (
