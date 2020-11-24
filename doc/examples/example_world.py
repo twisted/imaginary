bc2713d25e847f4357590c03e7b9ff1d364bf13e
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-11-23 19:16:54 -0500

    toss in a little color as long as I'm here, too

--- doc/examples/example_world.py
+++ doc/examples/example_world.py
@@ -19,12 +19,15 @@ def world(store):
         )
         Container.createFor(it, capacity=1000)
         return it
-    world = ImaginaryWorld(store=store,
-                           origin=room("The Beginning"))
+    origin = room("The Beginning", "Everything here looks fresh and new.")
+    world = ImaginaryWorld(store=store, origin=origin)
     protagonist = world.create("An Example Player")
     shirt = createShirt(store=store, name="shirt", location=world.origin)
     pants = createPants(store=store, name="pants", location=world.origin)
-    middle = room("The Middle")
+    middle = room(
+        "The Middle",
+        "At first glance, this appears to be the center of things.",
+    )
     wearer = IClothingWearer(protagonist)
     wearer.putOn(IClothing(shirt))
     wearer.putOn(IClothing(pants))
